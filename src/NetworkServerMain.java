1aefd6515627a2007725ddfa3ca2da8ff5f39a1c
ShigureChen <70284977+ShigureChen@users.noreply.github.com>
2021-03-06 01:14:56 +0800

    Added print IP and date time arguments

--- src/NetworkServerMain.java
+++ src/NetworkServerMain.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
 
 public class NetworkServerMain {
     public static void main(String args[]) throws IOException {
@@ -8,7 +9,10 @@ public class NetworkServerMain {
         ServerSocket sSocket = new ServerSocket(port);
         Socket socket = sSocket.accept();
 
-        
+        String IP = socket.getInetAddress().toString();
+        Date date = new Date();
 
+        System.out.println("Your IP address: " + IP);
+        System.out.println("Current date and time: " + date.getTime());
     }
 }
