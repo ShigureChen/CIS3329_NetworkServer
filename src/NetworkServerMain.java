15cde797b2d4dc46441dd9d965c15b3e5f2b760b
ShigureChen <70284977+ShigureChen@users.noreply.github.com>
2021-03-07 03:57:50 +0800

    Finished printing to browser arguments

--- src/NetworkServerMain.java
+++ src/NetworkServerMain.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Date;
@@ -6,13 +7,21 @@ import java.util.Date;
 public class NetworkServerMain {
     public static void main(String args[]) throws IOException {
         int port = 8000;
+
         ServerSocket sSocket = new ServerSocket(port);
         Socket socket = sSocket.accept();
 
         String IP = socket.getInetAddress().toString();
         Date date = new Date();
+        PrintWriter out = new PrintWriter(socket.getOutputStream());
 
-        System.out.println("Your IP address: " + IP);
-        System.out.println("Current date and time: " + date.toString());
+
+        out.println("HTTP/1.1 200 OK");
+        out.println("Content-Type: text/html");
+        out.println("\r\n");
+        out.println("<p> Your IP address: </p>" + IP);
+        out.println("<p> Current date and time: </p>" + date.toString());
+        out.flush();
+        out.close();
     }
 }
